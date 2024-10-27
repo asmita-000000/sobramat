b9ca9ccb8617fca01976df3dbf5031067fe18599
juanchoJRMP <juancho.jrmph@gmail.com>
2024-10-27 01:03:41 -0400

    subiendo filtros con la creacion de componente CategoryFilter.jsx y mosificacion de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Home from '../views/home';
 import UploadProduct from './components/uploadProduct';
 import DetailsProduct from './components/DetailsProduct';
 import EditProduct from './components/EditProduct';
-import ListMaterial from './components/listMaterial'; // Asegúrate de importar este componente
+import ListMaterial from './components/listMaterial';
+import CategoryFilter from './components/CategoryFilter';
 
 const App = () => {
   return (
@@ -13,7 +14,8 @@ const App = () => {
         <Route path="/upload" element={<UploadProduct />} />
         <Route path="/details/:id" element={<DetailsProduct />} />
         <Route path="/edit/:id" element={<EditProduct />} />
-        <Route path="/listMaterial" element={<ListMaterial />} /> {/* Nueva ruta */}
+        <Route path="/listMaterial" element={<ListMaterial />} />
+        <Route path="/Category" element={<CategoryFilter />} />
       </Routes>
     </div>
   );
